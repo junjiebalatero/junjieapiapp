beed1fb2d21c3f0faf3f05d08ae83107d25a5d15
margarito <margaritobalatero@gmail.com>
2020-09-21 09:07:03 +0800

    react-create-app on heroku

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{ 
+
+    constructor(props){
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+
+        fetch('https://junjienotes.herokuapp.com/api/posts')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+
+    }
+
+
+    render() {
+
+        var { isLoaded, items } = this.state;
+
+        if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
+
+        else {
+
+        return (
+            <div className="App">
+                
+                <ul>
+                    {items.map(item => (
+                        <li key={item.uid}>
+                            Date: {item.date} | Name: {item.name} | App: {item.app} | Username or Email: {item.username_or_email} | Password: {item.password} | Image Url: {item.imageUrl}
+
+                        </li>
+
+
+                        ))};
+
+
+                </ul>
+
+
+            </div>
+        );
+
+        }
+
+    }
+
 }
+
+
 
 export default App;
