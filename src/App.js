8083c50ca7ef1b2050cbedd23593b16058c913fe
marge <margaritobalatero@gmail.com>
2020-10-31 03:04:57 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends Component{
             <div className="App">                
                 <ul>
                     {items.map(item => (
-                        <li key={item._id}>
-                            Date Posted: {item.date} | Ravencoin Asset Name: {item.ravencoin_asset_name} | Total Circulating Supply: {item.total_circulating_supply} | Description: {item.description} | For Sale: {item.for_sale} | Contact Address: {item.contact_address} | Contact Email: {item.contact_email} | Contact Cellphone: {item.contact_cellphone} | Contact Phone: {item.contact_phone} | Nick Name: {item.nick_name} | Issuer: {item.issuer} | Website Url: {item.website_url} | IPFS Attachment: {item.IPFS_attachment} |
+                        <li key={item.uid}>
+                            Date: {item.date} | Ravencoin Asset Name: {item.ravencoin_asset_name} | App: {item.app} | Username or Email: {item.username_or_email} | Password: {item.password} | Image Url: {item.imageUrl}
                         </li>
                         ))};
                 </ul>                
