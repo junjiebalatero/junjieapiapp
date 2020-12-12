c8610539bbb0083c13884732823d6e61c2773893
marge <margaritobalatero@gmail.com>
2020-12-12 17:38:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -62,13 +62,14 @@ class App extends Component{
                     <h3>Use this backtrack to play Matud Nila by Ben Zubiri</h3>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/ap0J-GyuKkw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </ul>
-	        <ul>
+            </div>   
+	    
+	        <div>
 	        	<!-- Beginning of tippin.me Button -->
                         <div id="tippin-button" data-dest="JunjieBalatero" data-dark="true"></div>
                         <script src="https://tippin.me/buttons/tip.js?0001" type="text/javascript"></script>
                         <!-- End of tippin.me Button -->
-	        </ul>
-            </div>           
+	        </div>
         );
         }
     }
